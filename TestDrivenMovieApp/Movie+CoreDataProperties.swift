ca0601547429f165792eace236ffade09b737fc3
Håkon Bogen <bogen@beiningbogen.no>
2016-03-12 21:48:04 +0100

    Add movie model

---
+++ TestDrivenMovieApp/Movie+CoreDataProperties.swift
@@ -0,0 +1,20 @@
+//
+//  Movie+CoreDataProperties.swift
+//  TestDrivenMovieApp
+//
+//  Created by Håkon Bogen on 12/03/16.
+//  Copyright © 2016 hakonbogen. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Movie {
+
+    @NSManaged var name: String?
+    @NSManaged var movieID: Int32
+
+}
---
+++ TestDrivenMovieApp/Movie.swift
@@ -0,0 +1,17 @@
+//
+//  Movie.swift
+//  TestDrivenMovieApp
+//
+//  Created by Håkon Bogen on 12/03/16.
+//  Copyright © 2016 hakonbogen. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Movie: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
