b0160b061e86bc4effd8fce5bfab616c4529a5a9
Håkon Bogen <bogen@beiningbogen.no>
2016-03-12 20:56:23 +0100

    Initial Commit

---
+++ TestDrivenMovieAppTests/TestDrivenMovieAppTests.swift
@@ -0,0 +1,36 @@
+//
+//  TestDrivenMovieAppTests.swift
+//  TestDrivenMovieAppTests
+//
+//  Created by Håkon Bogen on 12/03/16.
+//  Copyright © 2016 hakonbogen. All rights reserved.
+//
+
+import XCTest
+@testable import TestDrivenMovieApp
+
+class TestDrivenMovieAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
